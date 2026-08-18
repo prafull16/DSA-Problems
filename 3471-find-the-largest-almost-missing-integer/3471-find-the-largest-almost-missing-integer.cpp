597388b1872926cf2aa68d561551c1d5a2420945
Prafull <prafullbaghel16@gmail.com>
2026-08-18 21:49:40 +0530

    Time: 0 ms (100%), Space: 29.1 MB (78.95%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        // Count frequency of every number
+        vector<int> freq(51, 0);
+
+        for (int x : nums) {
+            freq[x]++;
+        }
+
+        // Case 1: Only one subarray exists
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
+        }
+
+        // Case 2: Every element is a subarray
+        if (k == 1) {
+            int ans = -1;
+
+            for (int x : nums) {
+                if (freq[x] == 1) {
+                    ans = max(ans, x);
+                }
+            }
+
+            return ans;
+        }
+
+        // Case 3: 1 < k < n
+        int ans = -1;
+
+        // First element
+        if (freq[nums[0]] == 1) {
+            ans = max(ans, nums[0]);
+        }
+
+        // Last element
+        if (freq[nums[n - 1]] == 1) {
+            ans = max(ans, nums[n - 1]);
+        }
+
+        return ans;
+    }
+};
