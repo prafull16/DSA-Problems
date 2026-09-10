d8cd03752bf1da5325a1a2e90d87db43d7f305f0
Prafull <prafullbaghel16@gmail.com>
2026-09-10 18:14:09 +0530

    Time: 2 ms (53%), Space: 21.9 MB (69.55%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+        
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char ch = board[r][c];
+                if (ch == '.') continue;
+                
+                int num = ch - '1'; // 0-indexed digit
+                int boxIndex = (r / 3) * 3 + (c / 3);
+                
+                if (rows[r][num] || cols[c][num] || boxes[boxIndex][num]) {
+                    return false;
+                }
+                
+                rows[r][num] = true;
+                cols[c][num] = true;
+                boxes[boxIndex][num] = true;
+            }
+        }
+        
+        return true;
+    }
+};
