fa88c559c272ca693bf589d9e6b414aa71ad8f6b
Prafull <prafullbaghel16@gmail.com>
2026-09-20 15:17:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n < 3) return 0;
+        
+        vector<char> isComposite(n, 0);
+        int count = 1; // account for prime number 2
+        
+        for (int i = 3; i < n; i += 2) {
+            if (!isComposite[i]) {
+                count++;
+                if ((long long)i * i < n) {
+                    for (long long j = (long long)i * i; j < n; j += 2 * i) {
+                        isComposite[j] = 1;
+                    }
+                }
+            }
+        }
+        
+        return
