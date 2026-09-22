17ef58f6733d5109f4f70713db866a45f9d0e580
Prafull <prafullbaghel16@gmail.com>
2026-09-22 16:48:34 +0530

    Time: 90 ms (5.57%), Space: 13.3 MB (66.17%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        if(nums.size() == 1) return nums[0];
+
+        for(int i = 0 ; i < nums.size() ; i++){
+            
+            int cnt =  0;
+
+            for(int j = 0 ; j < nums.size() ; j++){
+
+                 if(nums[i] == nums[j]){
+                    cnt++;
+                 }
+            }
+
+            if(cnt == 1){
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
